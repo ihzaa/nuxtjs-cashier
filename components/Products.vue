b2f93b8a89143696992667d88d44cbe337d3cc35
Ihza Ahmad Abror Amrullah <ihzaahmad0@gmail.com>
2023-02-16 13:18:45 +0700

    9 Menu Categories

--- components/Products.vue
+++ components/Products.vue
@@ -1,22 +1,59 @@
 <template>
-  <v-row>
-    <v-col cols="2" v-for="(product, index) in products">
-      <v-card :title="product.title" :ripple="true">
-        <v-card-actions>
-          <v-img :src="require(`@/assets/images/products/${product.thumbnail}`)"></v-img>
-        </v-card-actions>
-        <v-card-text align="center" class="product-title">
-          {{ product.title }}
-        </v-card-text>
-      </v-card>
-    </v-col>
-</v-row>
+  <div>
+    <v-row>
+      <v-col cols="10">SEARCH</v-col>
+      <v-col cols="2">
+        <v-menu>
+          <template v-slot:activator="{ on: category }">
+            <v-btn v-on="category" color="primary">
+              Category
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item-group>
+              <v-list-item v-for="(category, index) in categories">
+                <v-list-item-title>
+                  {{ category.title }}
+                </v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-menu>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="2" v-for="(product, index) in       products">
+        <v-card :title="product.title" :ripple="true">
+          <v-card-actions>
+            <v-img :src="require(`@/assets/images/products/${product.thumbnail}`)"></v-img>
+          </v-card-actions>
+          <v-card-text align="center" class="product-title">
+            {{ product.title }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+</div>
 </template>
 
 <script>
 export default ({
   data() {
     return {
+      categories: [
+        {
+          id: 1,
+          title: 'HP',
+        },
+        {
+          id: 2,
+          title: 'Camera',
+        },
+        {
+          id: 3,
+          title: 'TV',
+        },
+      ],
       products: [
         { id: 1, title: 'Asus Zenfone', thumbnail: 'asus-zenfone.png', price: 2000000, categoryId: 1 },
         { id: 2, title: 'Canon Eos 700d', thumbnail: 'canon-eos-700d.png', price: 4300000, categoryId: 2 },
